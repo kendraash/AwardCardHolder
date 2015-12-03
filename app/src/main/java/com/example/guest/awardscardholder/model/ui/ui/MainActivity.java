8cff39a3f4fe1b2f0b6bc1c722a77f06398a1bb0
ka <student@epicodus.com>
2015-12-03 11:39:04 -0800

    add on click listener to move from main activity to category activity

--- app/src/main/java/com/example/guest/awardscardholder/model/ui/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/awardscardholder/model/ui/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.guest.awardscardholder.R;
@@ -14,13 +15,13 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @Bind(R.id.welcomeText) TextView welcomeText;
-    @Bind(R.id.signInText) TextView signInText;
-    @Bind(R.id.signInbutton) TextView signInButton;
-    @Bind(R.id.emailText) TextView emailText;
-    @Bind(R.id.passwordText) TextView paswordText;
-    @Bind(R.id.signUpButton) TextView signUpButton;
-    @Bind(R.id.newAwardsHolderText) TextView newAwardsHolderButton;
+    @Bind(R.id.welcomeText) TextView mWelcomeText;
+    @Bind(R.id.signInText) TextView mSignInText;
+    @Bind(R.id.signInbutton) Button mSignInButton;
+    @Bind(R.id.emailText) TextView mEmailText;
+    @Bind(R.id.passwordText) TextView mPaswordText;
+    @Bind(R.id.signUpButton) Button mSignUpButton;
+    @Bind(R.id.newAwardsHolderText) TextView mNewAwardsHolderButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +29,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String email = mEmailText.getText().toString();
+                String password = mPaswordText.getText().toString();
+                signIn(email, password);
+            }
+        });
     }
 
     public void signIn(String email, String password) {
         Intent intent = new Intent(this, CategoryActivity.class);
-        intent.putExtra(getString(R.string.key_name), email);
-        intent.putExtra(getString(R.string), password);
-
+        intent.putExtra("email", email);
+        intent.putExtra("password", password);
+        startActivity(intent);
     }
 
 }
